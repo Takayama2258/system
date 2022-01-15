3a472cdd5906a860d51931b3dd72ad64cfeb1c4b
coco <u3553498@hku.hk>
2022-01-15 17:15:10 +0800

    add context

---
+++ Downloads/system-master/src/App.js
@@ -0,0 +1,75 @@
+import './App.css';
+import Picture from './component/Picture';
+import {Breadcrumb, Button, Menu} from 'antd';
+import {CaretUpOutlined, CaretDownOutlined} from '@ant-design/icons'
+import ControlBar from "./component/ControlBar";
+import Layout, {Content} from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+
+import { AppContext } from "./component/AppContext";
+
+function App() {
+  const [label, setLabel] = useState([]);
+  const value = useMemo(
+    () => ({ label, setLabel }), 
+    [label]
+  );
+
+  return (
+      <div className="App">
+      <h2>Management System</h2>
+      <Layout style={{height:'100%',width:'100%'}}>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+          </Breadcrumb>
+          <Layout className="site-layout-background" style={{ padding: '30px 0' }}>
+            <Sider className="site-layout-background" width={400}>
+              <Menu
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  style={{ height: '100%' }}
+              >
+                <AppContext.Provider value={value}><ControlBar /></AppContext.Provider>
+              </Menu>
+            </Sider>
+            <Content className="container" style={{ padding: '0 16px', minHeight: 280}}>
+              <AppContext.Provider value={value}><Picture /></AppContext.Provider>
+            </Content>
+          </Layout>
+        </Content>
+      </Layout>
+      </div>
+  );
+
+
+
+
+
+
+  // return (
+  //   <div className="App">
+  //     <h2>Management System</h2>
+  //     <div className="left">
+  //       <div className="controlBar">
+  //         <ControlBar />
+  //       </div>
+  //     </div>
+  //     <div className="right">
+  //         {/*<div className="menu">*/}
+  //         {/*  <Button>source</Button>*/}
+  //         {/*  <Button>link to</Button>*/}
+  //         {/*  <Button>destination</Button>*/}
+  //         {/*</div>*/}
+  //         <div className="container">
+  //           <Picture />
+  //         </div>
+  //         {/*<div className="submit">*/}
+  //         {/*  <Button>Submit</Button>*/}
+  //         {/*</div>*/}
+  //     </div>
+  //   </div>
+  // );
+}
+
+export default App;
